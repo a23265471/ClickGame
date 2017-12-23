afcee0e6907fe305cb786f169498fb9c339b8ed0
4419-2-10\user <a23265471@gmail.com>
2017-12-23 10:49:29 +0800

    FadeIn/FadeOut

---
+++ ClickGame-Startup/Assets/Script/view/Behavior/EnimyBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnimyBehavior : MonoBehaviour {
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

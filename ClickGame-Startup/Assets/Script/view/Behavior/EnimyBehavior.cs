c37e546fb0185df9d8c30e985506e53944137aad
4419-2-10\user <a23265471@gmail.com>
2017-12-23 11:56:21 +0800

    health

--- ClickGame-Startup/Assets/Script/view/Behavior/EnimyBehavior.cs
+++ ClickGame-Startup/Assets/Script/view/Behavior/EnimyBehavior.cs
@@ -1,24 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(HealthComponent))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(AudioSource))]
 
 public class EnimyBehavior : MonoBehaviour {
 
     private Animator animator;
+    private MeshFader meshFader;
+    private AudioSource audioSource;
+    private HealthComponent healthComponent;
+    [SerializeField]
+    private AudioClip hurtClip;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<HealthComponent>();
+    }
+    
+    private void OnEnable()
+    {
+        StartCoroutine(meshFader.FadeIn());
+        healthComponent.Init(1000);
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void DoDamage(int attack)
+    {
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+        healthComponent.Hurt(attack);
+    }
+
+    private void Update()
+    {
+        if (healthComponent.IsOver)
+            return;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            DoDamage(10);
+        }
+
+    }
 }
